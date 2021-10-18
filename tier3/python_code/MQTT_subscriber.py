9e04da7a56153350419bcfdc119593a43e89326d
Tri Le-Viet <tri.leviet@protonmail.com>
2021-10-18 15:16:01 +1000

    progress

--- tier3/python_code/MQTT_subscriber.py
+++ tier3/python_code/MQTT_subscriber.py
@@ -1,10 +1,34 @@
 import paho.mqtt.client as mqtt
+import datetime as dt
 
 class MQTT_subscriber:
-    def __init__(self, port, lock, logger, username, password, topic):
+    def __init__(self, data, port, lock, logger, username, password, topic, updated):
         self.topic = topic
-        mqttc = mqtt.Client()
-        mqttc.username_pw_set(username=username, password=password)
+        self.data = data
+        self.updated = updated
+        self.mqttc = mqtt.Client()
+        self.mqttc.username_pw_set(username=username, password=password)
         certs_dir = "/etc/mosquitto/certs"
-        mqttc.tls_set(ca_certs=f"{certs_dir}/ca.crt", certfile=f"{certs_dir}server.crt", keyfile=f"{certs_dir}server.key")
-        mqttc.connect("localhost", port)
+        self.mqttc.tls_set(ca_certs=f"{certs_dir}/ca.crt", certfile=f"{certs_dir}server.crt", keyfile=f"{certs_dir}server.key")
+        self.mqttc.on_connect = self.subcscribe
+        self.mqttc.on_message = self.read
+        self.mqttc.connect("localhost", port)
+
+
+    def subcsribe(self, client, userdata, flags, rc):
+        self.mqttc.subscribe(self.topic)
+
+
+    def read(self, client, userdata, msg):
+        contents = msg.payload.decode('utf-8')
+        if contents == "WARNING_VALUE":
+            pass
+        elif contents == "WARNING_TIMESTAMP":
+            pass
+        else:
+            try:
+                value = float(contents)
+                self.data = [value, dt.datetime.now()]
+                self.updated.set()
+            except ValueError:
+                pass
--- tier3/python_code/data.py
+++ tier3/python_code/data.py
@@ -1,7 +1,19 @@
 import threading
-from MQTT_subscriber import MQTT_subscriber
+from MQTT_subscriber import *
 
 def collect_data(latest_data, update, engine, topics, port, lock, logger, username, password, name):
+    data = {}
+    subscribers = {}
+    lastUpdated = {}
     for topic in topics:
-        subscriber = MQTT_subscriber(port, lock, logger, username, password, f"{name}{topic}")
-        subscriber_thread = threading.Thread(target=subscriber)
+        lastUpdated[topic] = dt.datetime.now()
+        data[topic] = {}
+        subscriber = MQTT_subscriber(data[topic], port, lock, logger, username, password, f"{name}{topic}", threading.Event())
+        subscribers.append(subscriber)
+        #subscriber_thread = threading.Thread(target=subscriber)
+
+    while True:
+        for topic in topics:
+            if subscribers[topic].data[1] > lastUpdated[topic]:
+                pass
+                #
