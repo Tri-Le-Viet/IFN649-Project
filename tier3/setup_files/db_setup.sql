b8c1f862e7e60dfedd60f46eb61fbded6666d3cb
tle-viet <tri.leviet@protonmail.com>
2021-10-21 18:19:58 +1000

    updated db setup file

--- tier3/setup_files/db_setup.sql
+++ tier3/setup_files/db_setup.sql
@@ -1,18 +1,16 @@
-
-CREATE USER IF NOT EXISTS 'weather_storage'@'localhost' IDENTIFIED BY $USER_SQL_PASS;
 CREATE DATABASE weather;
 USE weather;
 
 CREATE TABLE stations (
   name VARCHAR(255) PRIMARY KEY,
-  lat DOUBLE(20, 18),
-  long DOUBLE(20, 18),
-)
+  latitude DOUBLE,
+  longitude DOUBLE
+);
 
 CREATE TABLE weather_data (
   time TIMESTAMP NOT NULL,
   station_name CHAR(3) NOT NULL,
-  temperature DECIMAL(3, 1),
+  temperature FLOAT,
   humidity FLOAT,
   heat_index FLOAT,
   dew_point FLOAT,
@@ -20,6 +18,11 @@ CREATE TABLE weather_data (
   average_wind_speed FLOAT,
   wind_gust FLOAT,
   warnings VARCHAR(255),
-  PRIMARY KEY (station_name, time)
-  FOREIGN KEY (station_name) REFERENCES stations (station_id)
-)
+  PRIMARY KEY (station_name, time),
+  FOREIGN KEY (station_name) REFERENCES stations (name)
+);
+
+INSERT INTO stations VALUES ("QUT Gardens Point", -27.477779504716224, 153.02954097170246);
+
+CREATE USER 'weather_storage'@'localhost' IDENTIFIED BY 'password';
+GRANT INSERT, SELECT ON weather.weather_data TO 'weather_storage'@'localhost';
